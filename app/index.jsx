581c6a27a8e2a7d09f1f40045e1aa0b772382e52
Santii05-C <santi.c.pgm@gmail.com>
2024-10-25 17:05:03 -0300

    last commit

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import { Image, Text, View, ScrollView } from "react-native";
-import { Link } from "expo-router";
+import { Redirect, router } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
-import CustomButton from "../components/CustomButton";
+
 import { images } from "../constants";
+import CustomButton from "../components/CustomButton";
 
 export default function App() {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: "100" }}>
-        <View className="w-full justify-center items-center h-full px-4">
+        <View className="w-full justify-center items-center min-h-[85vh] px-4">
           <Image
             source={images.logo}
             className="w-[130px] h-[84]"
@@ -42,11 +43,13 @@ export default function App() {
 
           <CustomButton
             title="Continue with Email"
-            handlePress={() => {}}
+            handlePress={() => router.push("/sign-in")}
             containerStyles="w-full mt-7"
           />
         </View>
       </ScrollView>
+
+      <StatusBar backgroundColor="#161622" style="light" />
     </SafeAreaView>
   );
 }
--- components/CustomButton.jsx
+++ components/CustomButton.jsx
@@ -10,9 +10,15 @@ const CustomButton = ({
 }) => {
   return (
     <TouchableOpacity
-      className={`bg-secondary rounded-xl min-h-[62px] justify-center items-center`}
+      onPress={handlePress}
+      activeOpacity={0.7}
+      className={`bg-secondary rounded-xl min-h-[62px] justify-center items-center ${containerStyles} ${
+        isLoading ? "opacity-50" : ""
+      } `}
     >
-      <Text className="text-primary font-psemibold text-lg">CustomButton</Text>
+      <Text className={`text-primary font-psemibold text-lg ${textStyles} `}>
+        {title}
+      </Text>
     </TouchableOpacity>
   );
 };
