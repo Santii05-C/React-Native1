538569051da8b82aeb945ac28feda42a176f774d
Santii05-C <santi.c.pgm@gmail.com>
2024-10-30 10:23:14 -0300

    last commit

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -2,7 +2,7 @@ import { View, Text, FlatList, Image } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-import { image, images } from "../../constants";
+import { images } from "../../constants";
 import SearchInput from "../../components/SearchInput";
 import Trending from "../../components/Trending";
 
@@ -10,14 +10,15 @@ const Home = () => {
   return (
     <SafeAreaView className="bg-primary">
       <FlatList
-        data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
+        // data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
+        data={[]}
         keyExtractor={(item) => item.$id}
         renderItem={({ item }) => (
           <Text className="text-3xl text-white">{item.id}</Text>
         )}
         ListHeaderComponent={() => (
-          <View className="my-6 px-4 space-y-6">
-            <View className="justify-between items-start flex-row mb-6">
+          <View className="flex my-6 px-4 space-y-6">
+            <View className="flex justify-between items-start flex-row mb-6">
               <View>
                 <Text className="font-pmedium text-sm text-gray-100">
                   Welcome Back
@@ -38,8 +39,8 @@ const Home = () => {
 
             <SearchInput />
 
-            <View className=" w-full flex-1 pt-5 pb-8">
-              <Text className="text-gray-100 text-lg font-pregular mb-3">
+            <View className="w-full flex-1 pt-5 pb-8">
+              <Text className="text-lg font-pregular text-gray-100 mb-3">
                 Latest Videos
               </Text>
 
@@ -47,6 +48,7 @@ const Home = () => {
             </View>
           </View>
         )}
+        ListEmptyComponent={() => <Text className="text-white">Empty</Text>}
       />
     </SafeAreaView>
   );
