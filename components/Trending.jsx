5262eff2ff76e7cb4730716f3dfaa4c2619389a1
Santii05-C <santi.c.pgm@gmail.com>
2024-10-31 18:20:40 -0300

    last commit

--- components/Trending.jsx
+++ components/Trending.jsx
@@ -1,6 +1,14 @@
 import { useState } from "react";
-import { View, Text, FlatList } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  ImageBackground,
+  Image,
+} from "react-native";
 import * as Animatable from "react-native-animatable";
+import { icons } from "../constants";
 
 const zoomIn = {
   0: {
@@ -20,12 +28,36 @@ const zoomOut = {
 };
 
 const TrendingItem = ({ activeItem, item }) => {
+  const [play, setPlay] = useState(false);
+
   return;
   <Animatable.View
     className="mr-5"
     animation={activeItem === item.$id ? zoomIn : zoomOut}
     duration={500}
-  ></Animatable.View>;
+  >
+    {play ? (
+      <Text className="text-white">Playing</Text>
+    ) : (
+      <TouchableOpacity
+        className="relative justify-center items-center"
+        activeOpacity={0.7}
+        onPress={() => setPlay(true)}
+      >
+        <ImageBackground
+          source={{ uri: item.thumbnail }}
+          className="w-52 h-72 rounded-[35] my-5 overflow-hidden shadow-lg shadow-black/40"
+          resizeMode="cover"
+        />
+        <Image
+          source={icons.play}
+          className="w-12 h-12 absolute"
+          resizeMode="contain"
+        />
+        //2:55
+      </TouchableOpacity>
+    )}
+  </Animatable.View>;
 };
 
 const Trending = ({ posts }) => {
