efa94f16d6de00f4b8a56921f46034fad6d4d13f
Santii05-C <santi.c.pgm@gmail.com>
2024-11-02 10:34:01 -0300

    last commit

--- app/search/[query].jsx
+++ app/search/[query].jsx
@@ -1,16 +1,16 @@
+import { useEffect } from "react";
+import { useLocalSearchParams } from "expo-router";
 import { View, Text, FlatList } from "react-native";
-import React, { useEffect } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-import SearchInput from "../../components/SearchInput";
+import useAppwrite from "../../lib/useAppwrite";
+import { searchPosts } from "../../lib/appwrite";
 import EmptyState from "../../components/EmptyState";
 import VideoCard from "../../components/VideoCard";
-import { searchPosts } from "../../lib/appwrite";
-import useAppwrite from "../../lib/useAppwrite";
-import { useLocalSearchParams } from "expo-router";
+import SearchInput from "../../components/SearchInput";
 
 const Search = () => {
-  const { query } = useLocalSearchParams;
+  const { query } = useLocalSearchParams();
   const { data: posts, refetch } = useAppwrite(() => searchPosts(query));
 
   useEffect(() => {
@@ -24,21 +24,25 @@ const Search = () => {
         keyExtractor={(item) => item.$id}
         renderItem={({ item }) => <VideoCard video={item} />}
         ListHeaderComponent={() => (
-          <View className="flex my-6 px-4 ">
-            <Text className="font-pmedium text-sm text-gray-100">
-              Search Result
-            </Text>
-            <Text className="text-2xl font-psemibold text-white">{query}</Text>
+          <>
+            <View className="flex my-6 px-4">
+              <Text className="font-pmedium text-gray-100 text-sm">
+                Search Results
+              </Text>
+              <Text className="text-2xl font-psemibold text-white mt-1">
+                {query}
+              </Text>
 
-            <View className="mt-6 mb-6">
-              <SearchInput initialQuery={query} />
+              <View className="mt-6 mb-8">
+                <SearchInput initialQuery={query} />
+              </View>
             </View>
-          </View>
+          </>
         )}
         ListEmptyComponent={() => (
           <EmptyState
             title="No Videos Found"
-            subtitle="No video found for this search query"
+            subtitle="No videos found for this search query"
           />
         )}
       />
