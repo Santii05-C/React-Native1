6561f354a6d68694af894103abe84cc497495309
Santii05-C <santi.c.pgm@gmail.com>
2024-11-04 15:36:59 -0300

    last commit

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,12 +1,12 @@
-import { View, Text, FlatList, TouchableOpacity } from "react-native";
-import React, { useEffect } from "react";
+import { View, FlatList, TouchableOpacity } from "react-native";
+import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import SearchInput from "../../components/SearchInput";
+import InfoBox from "../../components/InfoBox";
 import EmptyState from "../../components/EmptyState";
 import VideoCard from "../../components/VideoCard";
 import { getUserPosts } from "../../lib/appwrite";
 import useAppwrite from "../../lib/useAppwrite";
-import { useLocalSearchParams } from "expo-router";
 
 import { useGlobalContext } from "../../context/GlobalProvider";
 import { Image } from "react-native";
@@ -44,6 +44,26 @@ const Profile = () => {
                 source={{ uri: user?.avatar }}
                 className="w-[90%] h-[90%] rounded-lg"
                 resizeMode="cover"
+              />
+            </View>
+
+            <InfoBox
+              title={user?.username}
+              containerStyles="mt-5"
+              titleStyles="text-lg"
+            />
+
+            <View className="mt-5 flex-row">
+              <InfoBox
+                title={posts.length || 0}
+                subtitle="Posts"
+                containerStyles="mr-10"
+                titleStyles="text-xl"
+              />
+              <InfoBox
+                title="1.2k"
+                subtitle="Followers"
+                titleStyles="text-xl"
               />
             </View>
           </View>
